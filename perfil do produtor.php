8f5fcde2a4bc45086ff6d53c2828fe6a37f5a3b5
andrei817 <andreieluiz234@gmail.com>
2024-11-19 03:31:31 -0300

    Update perfil do produtor.php

--- perfil do produtor.php
+++ perfil do produtor.php
@@ -147,7 +147,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
    </script>
 
 
-<section class="agenda"> 
+<div class="agenda-evento">
+    <div class="conteudo">
     <section class="login-section"> 
 
     <div class="login-box">
