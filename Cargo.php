66cc5075e916dcbcce6646e8dff8474b9b103d0f
andrei817 <andreieluiz234@gmail.com>
2024-11-27 09:14:11 -0300

    Update Cargo.php

--- Cargo.php
+++ Cargo.php
@@ -1,11 +1,28 @@
 <?php
 session_start();
-include ("php/Config.php");
+include("php/Config.php");
 
-// Consulta para pegar todos os cargos de staff
-$sql = "SELECT * FROM cargos";
-$result = $conn->query($sql);
+// Definir o número de registros por página
+$registros_por_pagina = 3;
+
+// Determinar a página atual (padrão: 1)
+$pagina_atual = isset($_GET['pagina']) && is_numeric($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
+$offset = ($pagina_atual - 1) * $registros_por_pagina;
+
+// Consulta para obter o total de registros
+$total_result = $conn->query("SELECT COUNT(*) AS total FROM cargos");
+$total_row = $total_result->fetch_assoc();
+$total_registros = $total_row['total'];
+$total_paginas = ceil($total_registros / $registros_por_pagina);
+
+// Consulta com LIMIT e OFFSET para a página atual
+$sql = "SELECT * FROM cargos LIMIT ? OFFSET ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ii", $registros_por_pagina, $offset);
+$stmt->execute();
+$result = $stmt->get_result();
 ?>
+
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -60,20 +77,18 @@ function fecharSidebar() {
 }
 
     </script>
-
 <section class="cargo-evento">
      <div class="conteudo">
-
-    <h2>Cargos</h2> 
+    <h2>CARGOS</h2> 
      <a href="adcionar_cargo.php" class="button">Adicionar Novo Cargo</a>
      </div>
 
-    <table border="1">
+    <table>
         <thead>
             <tr>
                 <th class="id-column">ID</th>
-                <th>Cargo</th>
-                <th>Ações</th>
+                <th class="itens">Cargo</th>
+                <th class="ações">Ações</th>
             </tr>
         </thead>
         <tbody>
@@ -82,11 +97,11 @@ function fecharSidebar() {
                 <td class="id-column"><?php echo $cargo['id']; ?></td>
                 <td><?php echo $cargo['nome']; ?></td>
                 <td class="cargo-action">
-                    <a href="edit cargo.php?id=<?php echo $cargo['id']; ?>"><button class='btn-edit-cargo'> <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil-square' viewBox='0 0 16 16'>
+                    <a class="a" href="edit cargo.php?id=<?php echo $cargo['id']; ?>"><button class='btn-edit-cargo'> <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil-square' viewBox='0 0 16 16'>
 <path d='M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z'/>
 <path fill-rule='evenodd' d='M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z'/> </a> </svg> </button>
 </a> 
-                    <a href="delete cargo.php?id=<?php echo $cargo['id']; ?>" onclick="return confirm('Tem certeza que deseja excluir este cargo?');"><button class='btn-delete-cargo' > <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                    <a class="a" href="delete cargo.php?id=<?php echo $cargo['id']; ?>" onclick="return confirm('Tem certeza que deseja excluir este cargo?');"><button class='btn-delete-cargo' > <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
 <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
 </svg> </a> </button></a>
                 </td>
@@ -95,7 +110,15 @@ function fecharSidebar() {
         </tbody>
     </table>
 
-    <br>
+      <!-- Paginação -->
+      <div class="pagination">
+        <?php for ($i = 1; $i <= $total_paginas; $i++): ?>
+            <a href="?pagina=<?php echo $i; ?>" class="<?php echo $pagina_atual == $i ? 'active' : ''; ?>">
+                <?php echo $i; ?>
+            </a>
+        <?php endfor; ?>
+    </div>
+    
 </section>
 </body>
 </html>
