dc4a1232622776f7fa9627a584f0b532d3cdb9a8
andrei817 <andreieluiz234@gmail.com>
2024-12-03 01:25:37 -0300

    Update ambiente.php

--- ambiente.php
+++ ambiente.php
@@ -56,7 +56,7 @@ $showModal = true;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+    <link rel="shortcut icon" href="ico/SGE.ico" type="image/x-icon">
     <title>SGE - Ambiente do Produtor</title>
     <link
     rel="stylesheet"
