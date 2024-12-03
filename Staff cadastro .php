eb02cdcd33bf3f6dc50a257084c9213f975da47c
andrei817 <andreieluiz234@gmail.com>
2024-12-03 01:31:59 -0300

    Update Staff cadastro .php

--- Staff cadastro .php
+++ Staff cadastro .php
@@ -32,6 +32,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="ico/SGE.ico" type="image/x-icon">
     <title>SGE - Cadastro do Staff</title>
     <link rel="stylesheet" href="Staff.css">
     <script src="evento.js"> </script>
